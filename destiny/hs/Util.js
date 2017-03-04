c0605d1e194fa2ffa4d44b92cc5ec30064977436
Aleksei Chernykh <Aleksei.Chernykh@williamhill.com.au>
2017-03-04 14:35:53 +1100

    Logging improvements - support for circular references and colored logging

--- destiny/hs/Util.js
+++ destiny/hs/Util.js
@@ -8,13 +8,28 @@ module.exports = {
 	    return str.replace(/\{(\d+)\}/g, function(v) {
 	    	var i = parseInt(v.substring(1));
 	    	if (args[i + offset] !== undefined) {
-	    		return args[i + offset];
+	    		var arg = args[i + offset];
 	    		if (typeof arg == "object") {
+	    			var cache = [];
+	    			// handles circular references
+	    			var replacer = function(key, value) {
+	    				if (typeof value === 'object' && value !== null) {
+	    					if (cache.indexOf(value) !== -1) {
+	    						// Circular reference found, discard key
+	    						return;
+	    					}
+	    					// Store value in our collection
+	    					cache.push(value);
+	    				}
+	    				return value;
+	    			};
+
 	    			if (sails.config.destiny.logPrettyFormat == true) {
-	    				arg = JSON.stringify(arg, true, 2);
+	    				arg = JSON.stringify(arg, replacer, 2);
 	    			} else {
-	    				arg = JSON.stringify(arg);
+	    				arg = JSON.stringify(arg, replacer);
 	    			}
+	    			cache = null; // Enable garbage collection
 	    		}
 	    		return arg;
 	    	} else {
